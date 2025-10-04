aedfa95479a00ab046b4bed6f2fb0d082eb18551
Tanmay Islam <tanmayislam101@gmail.com>
2025-10-04 23:03:11 +0600

    solve count vowels

--- index.js
+++ index.js
@@ -54,3 +54,7 @@ function simpleCalculator(a, b, operation) {
       return undefined;
   }
 }
+function countVowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
