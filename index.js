119c9feac55f3000fe2523cb439f0a4dead948ef
Tanmay Islam <tanmayislam101@gmail.com>
2025-10-05 19:09:10 +0600

    solve the title case

--- index.js
+++ index.js
@@ -68,3 +68,10 @@ function findLongestWord(str) {
   }
   return longestWord;
 }
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
