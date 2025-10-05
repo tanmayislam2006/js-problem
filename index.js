eafdb6032f0dcbd0ecd23f89813edf61338f88de
Tanmay Islam <tanmayislam101@gmail.com>
2025-10-05 19:24:45 +0600

    solve Array Sum

--- index.js
+++ index.js
@@ -75,3 +75,6 @@ function titleCase(str) {
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
+function arraySum(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
