242578d200cbf60126634bad929cc46ede4d19ec
Tanmay Islam <tanmayislam101@gmail.com>
2025-10-09 20:35:46 +0600

    solve complete two sum

--- index.js
+++ index.js
@@ -90,3 +90,14 @@ function findMissingNumber(arr, n) {
   const actualSum = arr.reduce((acc, curr) => acc + curr, 0);
   return expectedSum - actualSum;
 }
+function twoSum(arr, target) {
+  const numMap = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+    numMap.set(arr[i], i);
+  }
+  return null;
+}
