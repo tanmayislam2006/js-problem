b9e3343a4fd69c02995d6eee7d9711cd03158c76
Tanmay Islam <tanmayislam101@gmail.com>
2025-10-15 22:17:11 +0600

    solve Clock Angle problem

--- index.js
+++ index.js
@@ -101,3 +101,15 @@ function twoSum(arr, target) {
   }
   return null;
 }
+// Clock Angle
+function twoSum(arr, target) {
+  const numMap = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+    numMap.set(arr[i], i);
+  }
+  return null;
+}
