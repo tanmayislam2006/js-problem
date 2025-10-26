ea14138c104f237e16a4f083df6f702da39d88b4
Tanmay Islam <tanmayislam101@gmail.com>
2025-10-26 11:00:36 +0600

    add throttle function for rate limiting

--- index.js
+++ index.js
@@ -122,3 +122,23 @@ function debounce(func, delay) {
     }, delay);
   };
 }
+// Throttle Function
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function(...args) {
+    const context = this;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
